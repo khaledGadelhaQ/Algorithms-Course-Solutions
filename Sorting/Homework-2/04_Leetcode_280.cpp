609e49b8ef445de24d3d041004ea02c51360e829
khaledgad <khaledgadelhaq@gmail.com>
2023-09-24 22:34:06 +0200

    Better solution

--- Sorting/Homework-2/04_Leetcode_280.cpp
+++ Sorting/Homework-2/04_Leetcode_280.cpp
@@ -26,17 +26,15 @@ public:
 
 class Solution2{
 public:
+    
     //  O(n) time , O(1) memory
     void wiggleSort(vector<int> &nums) {
+
         if(nums.size() == 0) return;
 
-
-        for(int i = 1 ; i < nums.size()-1 ; i++){
-            if(i%2 == 0 && nums[i] > nums[i+1]){
-                swap(nums[i+1],nums[i]);i++;
-            }
-            if(i%2 == 1 && nums[i] < nums[i+1]){
-                swap(nums[i+1],nums[i]);i++;
+        for(int i = 0 ; i < nums.size()-1 ; i++){
+            if( (i%2 == 0 && nums[i] > nums[i+1]) || ( i%2 == 1 && nums[i] < nums[i+1]) ){
+                swap(nums[i+1],nums[i]); 
             }
         }
         
@@ -44,22 +42,15 @@ public:
 };
 
 int main(){
-    vector<int> v = {3,5,2,1,6,4};
     vector<int> v2 = {3,5,2,1,6,4};
-    Solution s;
     Solution2 s2;
-    s.wiggleSort(v);
     s2.wiggleSort(v2);
-    for(auto val : v ){
-        cout << val << " ";
-    }
-    cout << endl;
     for(auto val : v2 ){
         cout << val << " ";
     }
     cout << endl;
 
-    cout <<"3,5,1,6,2,4";
+    cout <<"3 5 1 6 2 4";
     return 0 ;
 }
 
