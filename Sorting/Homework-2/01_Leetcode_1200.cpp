ea74520b9497cb35a21a5e36bf503dabd83decf4
khaledgad <khaledgadelhaq@gmail.com>
2023-09-24 12:37:18 +0200

    adding new files

---
+++ Sorting/Homework-2/01_Leetcode_1200.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        int min {INT_MAX};
+        for(int i =1 ; i < arr.size() ; i++){
+            int diff = abs(arr[i] - arr[i-1]) ;  
+
+            if( diff > min){continue;}
+            if( diff == min){
+               ans.push_back({arr[i-1] , arr[i]});
+            }else{
+                ans.clear();
+                min = diff;
+                ans.push_back({arr[i-1] , arr[i]});
+            }
+        }
+        return ans;
+
+    }
+};
+
+int main() {
+
+	return 0;
+}
